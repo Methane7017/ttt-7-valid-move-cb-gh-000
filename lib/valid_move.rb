a665d6a4077d3470767a0c34df6a2cd375ff8b4d
Vishal D Wavhale <nanocarbon7017@gmail.com>
2019-12-28 16:23:41 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,24 @@
-def position_taken?(board[], index)
-if (board[index] == " " || board[index] == "" || board[index] == nil)
+
+def valid_move?(board, position)
+  spot = position.to_1 - 1
+  if !position_taken?(board, spot) && spot.between?(0,8)
+    true
+  else
+    false
+  end
+end
+
+
+
+
+
+
+
+
+def position_taken?(board, index)
+  if (board[index] == " " || board[index] == "" || board[index] == nil)
     taken = false
   else
     taken = true
+  end
 end
