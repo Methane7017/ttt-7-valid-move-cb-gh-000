7aa1b7e6e03d3b82b274298a7cb23994f51edd13
Vishal D Wavhale <nanocarbon7017@gmail.com>
2019-12-28 16:24:57 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -15,7 +15,7 @@ end
 
 
 
-def position_taken?(board, postion)
+def position_taken?(board, position)
   if (board[position] == " " || board[position] == "" || board[position] == nil)
     false
   else
